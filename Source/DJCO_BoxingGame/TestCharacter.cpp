25c20a4c48172e19605e2429f25e12f4d1cb3d48
jpmmaia <jpmmaia@gmail.com>
2016-02-22 15:40:23 +0000

    Adding test character.

---
+++ Source/DJCO_BoxingGame/TestCharacter.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DJCO_BoxingGame.h"
+#include "TestCharacter.h"
+
+
+// Sets default values
+ATestCharacter::ATestCharacter()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATestCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATestCharacter::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+// Called to bind functionality to input
+void ATestCharacter::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+{
+	Super::SetupPlayerInputComponent(InputComponent);
+
+}
+
